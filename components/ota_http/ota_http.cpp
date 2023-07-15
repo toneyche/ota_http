b5bc3b06fc0d27ca57af564cfc29ac5cdf5037c2
toneyche <43717356+toneyche@users.noreply.github.com>
2023-07-14 19:10:05 -0700

    Update ota_http.cpp

--- components/ota_http/ota_http.cpp
+++ components/ota_http/ota_http.cpp
@@ -108,7 +108,7 @@ void OtaHttpComponent::flash() {
   const char *headerKeys[] = {"Content-Length", "Content-Type"};
   const size_t headerCount = sizeof(headerKeys) / sizeof(headerKeys[0]);
   size_t total_size;
-  const size_t chunk_size = 1024*600;  // HTTP_TCP_BUFFER_SIZE;
+  const size_t chunk_size = 1024;  // HTTP_TCP_BUFFER_SIZE;
   size_t chunk_start = 0;
   size_t chunk_stop = chunk_size;
   size_t chunk_end;
@@ -195,6 +195,7 @@ void OtaHttpComponent::flash() {
     md5_receive.add(buf, bufsize);
 
     update_started = true;
+    ESP_LOGE(TAG, "begin write");
     error_code = backend->write(buf, bufsize);
     if (error_code != 0) {
       esphome::ESP_LOGW(TAG, "Error code (%d) writing binary data to flash at offset %d/%d and size %s", error_code,
